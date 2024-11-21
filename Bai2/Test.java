8e914c6fc4b2f12f3a976cb59b031a48d20dddef
NguyenKimHoangLong <longpro0912@gmail.com>
2024-11-21 23:18:49 +0700

    baitap

---
+++ Bai2/Test.java
@@ -0,0 +1,18 @@
+package Bai2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DiaChi dc = new DiaChi();
+		dc.setThon("chau phong");
+		dc.setXa("lien ha");
+		dc.setHuyen("dong anh");
+		dc.setTinh("ha noi");
+		dc.getThon();
+		dc.getXa();
+		dc.getHuyen();
+		dc.getTinh();
+		System.out.println(dc);
+	}
+
+}
